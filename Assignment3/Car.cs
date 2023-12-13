b6dadb5a1ca0eb8d17886d067eb62c43a3bd7b55
Aaishalondt20 <5100170@mypillar7uwc.ac.za>
2023-12-13 06:59:15 +0200

    Add project files.

---
+++ Assignment3/Car.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+   
+    
+        internal class Car
+        {
+            public int NoOfWheels;
+            public int NoOfDoors;
+            public double speed;
+            public bool HasGps;
+            double FuelLevel;
+            public string VinNumber;
+            public string LicenseNumber;
+
+            public bool HasRegistrationLicense;
+
+            public Driver CurrentDriver = new Driver();
+
+
+            public Car(int noOfWheels, int noOfDoors, bool hasGps, bool hasRegistrationLicense, string vinNumber, Driver driver, string licenseNumber)
+        {
+            NoOfWheels = noOfWheels;
+            NoOfDoors = noOfDoors;
+            HasGps = hasGps;
+            HasRegistrationLicense = hasRegistrationLicense;
+            VinNumber = vinNumber;
+            CurrentDriver = driver;
+            LicenseNumber = licenseNumber;
+        }
+
+
+         public void StartEngine()
+            {
+                speed = 1;
+            }
+
+            public void StopEngine()
+            {
+                speed = 0;
+            }
+
+            public void Accelerate(double speedAmount)
+            {
+                speed += speedAmount;
+            }
+
+            public void Decelerate(double speedAmount)
+            {
+                speed -= speedAmount;
+
+                if (speed < 0)
+                {
+                    speed = 0;
+                }
+            }
+
+            public double GetCurrentSpeed()
+            {
+                return speed;
+            }
+
+        }
+    }
+
+
---
+++ Assignment3/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Assignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Driver driver = new Driver();
+            driver.Name = "Aaisha";
+            driver.HasDriversLicense = false;
+
+
+            Car BMW = new Car(4, 5, true, true, "AL98AD4509", driver,"CA200803");
+            BMW.StartEngine();
+            BMW.Accelerate(50);
+
+            Console.WriteLine("BMW: " + BMW.speed);
+
+            Console.WriteLine("BMW VIN? :" +  BMW.VinNumber);
+            Console.WriteLine("BMW DOORS? : " + BMW.NoOfDoors);
+            Console.WriteLine("BMW HAVE GPS? : " + BMW.HasGps);
+            BMW.StopEngine();
+            Console.WriteLine("BMW Driver? : " + BMW.CurrentDriver.Name);
+            Console.WriteLine("BMW Driver License? : " + BMW.CurrentDriver.HasDriversLicense);
+            Console.WriteLine("BMW Driver License Number?" + BMW.CurrentDriver.LicenseNumber);
+        }
+    }
+}
